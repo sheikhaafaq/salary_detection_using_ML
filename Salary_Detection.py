9a28f661c2231fe67697331d88136b9e5ec9b6fb
sheikh aafaq <sheikhaafaq18@gmail.com>
2021-05-27 01:35:23 +0530

    latest

--- Salary_Detection.py
+++ Salary_Detection.py
@@ -1,38 +1,9 @@
-import pandas, joblib
-
-#Load datset
-ds = pandas.read_csv('salary.csv')
-
-ds.columns
-ds.info()
-x = ds['YearsExperience'].values.reshape(30,1)
-type(x)
-x.shape
-y = ds['Salary']
-
-# Train model using LinearRegression algorithm
-from sklearn.linear_model import LinearRegression
-
-# Create model
-model = LinearRegression()
-
-# Fit features
-model.fit(x , y)
-
-# Get weight
-model.coef_
-
-# Predict results
-model.predict([[ 1.1 ]] )
-
-# Get Bias
-model.intercept_
-
-#save model
-joblib.dump(model, "salary_model.pki")
+import  joblib
 
 #load model
-model2 = joblib.load('model.pki')
+model = joblib.load('salary_model.pki')
 
 #predict
-model.predict([[ 1.1 ]] )
+x = model.predict([[ float(input("How much yearsOfExperience you have? ") ) ]] )
+
+print(f"Your salary should be Rs { x }")
