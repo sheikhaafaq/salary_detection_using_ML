0564f168b98ea8317f4133538a79c590ec81156f
Sheikh Aafaq Rashid <56719749+sheikhaafaq@users.noreply.github.com>
2021-05-27 01:46:23 +0530

    Update Salary_Detection.py

--- Salary_Detection.py
+++ Salary_Detection.py
@@ -6,4 +6,4 @@ model = joblib.load('salary_model.pki')
 #predict
 x = model.predict([[ float(input("How much yearsOfExperience you have? ") ) ]] )
 
-print(f"Your salary should be Rs { x }")
+print(f"Your salary should be Rs { x[0] }")
